78e13d9808b929758e0c1b715ff4d9ceecba83c5
Vladimir <vladimir27goncharov@icloud.com>
2018-10-10 23:07:26 +0300

    Реализована функция EncryptCaesar

--- homework01/caesar.go
+++ homework01/caesar.go
@@ -2,9 +2,20 @@ package caesar
 
 func EncryptCaesar(plaintext string, shift int) string {
 	var ciphertext string
+	var symbol int
 
 	// PUT YOUR CODE HERE
-
+	ciphertext=""
+	for i:=0; i<len(plaintext);i++{
+		symbol=int(plaintext[i])
+		if (int('A')<=symbol)&&(int('Z')>=symbol) || (int('a')<=symbol)&&(int('z')>=symbol){
+			symbol+=shift%26
+		}
+		if (symbol>int('Z')) && (symbol<int('a')) || (symbol>int('z')){
+			symbol-=26
+		}
+		ciphertext+=string(symbol)
+	}
 	return ciphertext
 }
 
