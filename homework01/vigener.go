660957fa63a29a38723c7c0c077e6f3467621754
Vladimir <vladimir27goncharov@icloud.com>
2018-10-13 16:22:14 +0300

    Реализована функция EncryptVigenere()

--- homework01/vigener.go
+++ homework01/vigener.go
@@ -5,6 +5,28 @@ func EncryptVigenere(plaintext string, keyword string) string {
 
 	// PUT YOUR CODE HERE
 
+	var i,j int
+	i=0
+	for j=0; j<len(plaintext);j++{
+		var let int
+		let=int(plaintext[j])
+		if i>=len(keyword){
+			i=0
+		}
+		if (int('A') <= let) && (let <= int('Z')) || (int('a') <= let && let <= int('z')){
+			if (int('A') <= int(keyword[i])) && (int(keyword[i])<=int('Z')) {
+				let = let + int(keyword[i]) - int('A')
+			}else{
+				let = let + int(keyword[i]) - int('a')
+			}
+		}
+		if (int('Z')<let) && (int('a')>let) || (int('z')<let){
+			let-=26
+		}
+		i+=1
+		ciphertext+=string(let)
+	}
+
 	return ciphertext
 }
 
