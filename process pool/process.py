d87ee898d181a10b7e045706e31ad8bd8febe88f
Vladimir <vladimir27goncharov@icloud.com>
2019-03-14 23:20:32 +0300

    Pool with example of heavy computation func inside

---
+++ process pool/process.py
@@ -0,0 +1,70 @@
+import os
+from time import sleep
+from multiprocessing import Process, Queue, Manager
+import psutil
+import random
+
+
+class ProcessPool:
+    def __init__(self, min_workers=2, max_workers=10, mem_usage=1024):
+        self.min_workers = min_workers
+        self.max_workers = max_workers
+        self.mem_usage = mem_usage
+        self.process_list = []
+        self.process_ram = 0
+
+    def mem_test(self, func, data):
+        return_dict = Manager().dict()
+        proc = Process(target=func, args=(data,))
+        proc.start()
+        proc1 = Process(target=self.measure_ram, args=(proc.pid, return_dict))
+        proc1.start()
+        proc.join()
+        proc1.join()
+        self.process_ram = return_dict['process_ram']
+
+    def measure_ram(self, pid, return_dict):
+        max_ram = 0
+        try:
+            while psutil.pid_exists(pid):
+                result = psutil.Process(pid).memory_info().rss / 1024 ** 2
+                if result > max_ram:
+                    max_ram = result
+                print(psutil.cpu_percent(interval=1, percpu=True))
+        except psutil.NoSuchProcess:
+            pass
+        return_dict['process_ram'] = max_ram
+
+    def map(self, func, bid_data: Queue):
+        self.mem_test(func, bid_data.get())
+        print(self.process_ram)
+        process_amount = int(self.mem_usage / self.process_ram)
+        print(process_amount)
+        procs = []
+        for _ in range(process_amount):
+            proc = Process(target=func, args=(bid_data.get(),))
+            procs.append(proc)
+            proc.start()
+        while not bid_data.empty():
+            for idx, proc in enumerate(procs):
+                if not proc.is_alive():
+                    new_proc = Process(target=func, args=(bid_data.get(),))
+                    procs[idx] = new_proc
+                    print(big_data)
+        return process_amount, memory_result
+
+def doubler(a):
+    array = [random.randint(0, 100) for _ in range(2000000)]
+    array.sort()
+    proc = psutil.Process(os.getpid())
+    print(os.getpid())
+    print('RAM in \'doubler\' func: ', proc.memory_info().rss / 1024 / 1024)
+
+
+if __name__ == '__main__':
+    pool = ProcessPool(2, 10, 1024)
+    q = Queue()
+    for _ in range(100):
+        q.put(1)
+    a, b = pool.map(doubler, q)
+    print(a, b)
